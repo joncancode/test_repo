2f2044e71e4d9870948a2ca77f34a6fe2cb45537
joncancode <jc.haines19@gmail.com>
2017-06-12 16:17:15 -0400

    finished 8 problems

--- w1d1-exercises-part-1.js
+++ w1d1-exercises-part-1.js
@@ -1,7 +1,40 @@
-function whoAmI(name, age) {
-    const yearOfBirth = 2017 - age;
-    console.log(`Hi my name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in ${yearOfBirth}`);
+function yearOfBirth(age) {
+	
+	if (age < 0) {
+		throw new Error("age cannot be negative")
+	}
+	if (isNaN(age)) {
+		throw new Error("age must be a number")
+	}
+	return new Date().getFullYear() - age
 }
 
-whoAmI();
+function displayAge(age) {
+	try {
+		console.log(`You were born in ${yearOfBirth(age)}`)
+	} catch(err) {
+		console.log(`There is an error: ${err.message}`);
+	}
+}
+
+function nameOfPerson(name) {
+	if (typeof name === 'string') {
+		console.log("all is good")
+	} else {
+		console.log("didn't work")
+	}
+}
+
+nameOfPerson(22)
+
+console.log(yearOfBirth("3"));
+
+
+function whoAmI(name, age) {
+    const yearOfBirth = new Date().getFullYear() - age;
+
+    console.log(`Hi my name is ${name} and I'm ${age} years old`);
+}
+	
+
+whoAmI("Bob", 12);
