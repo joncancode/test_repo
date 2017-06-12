f968ea08612d69fd6b21465dbbcdf7b228c4fa92
Kristoffer Panahon <krp312@me.com>
2017-06-12 15:20:34 -0400

    first half of the part 1 js exercises

---
+++ w1d1-exercises-part-1.js
@@ -0,0 +1,7 @@
+function whoAmI(name, age) {
+    const yearOfBirth = 2017 - age;
+    console.log(`Hi my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
+}
+
+whoAmI();
